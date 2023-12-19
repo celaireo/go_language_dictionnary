973b0651e8f9f9ecf3ed45a12687f4a6c869c839
celaireo <celaireo@gmail.com>
2023-12-19 18:11:22 +0100

    Creation du package dictionary avec les methodes Get, Add, Remove et List

--- main.go
+++ main.go
@@ -2,36 +2,76 @@ package main
 
 import (
 	"fmt"
+	"dictionnaire/dictionary"
 )
 
 func main() {
-	// Créer une carte pour stocker les villes et les pays
-	dico_villes := make(map[string]string)
+	// Créer une nouvelle instance de votre dictionnaire
+	dico := dictionary.NewDictionary()
 
-	// Ajouter des éléments à la carte
-	dico_villes["paris"] = "France"
-	dico_villes["lisbonne"] = "Portugal"
-	dico_villes["londres"] = "Angleterre"
+	// Ajouter des éléments au dictionnaire
+	dico.AddWord("paris", "France")
+	dico.AddWord("lisbonne", "Portugal")
+	dico.AddWord("londres", "Angleterre")
 
-	// Afficher la carte
-	fmt.Println("Carte après ajout:", dico_villes)
+	// Afficher le dictionnaire
+	fmt.Println("Dictionnaire après ajout:")
+	dico.PrintDictionary()
 
 	// Utiliser la méthode Get pour obtenir la définition d'un mot spécifique
-	if definition, exists := dico_villes["paris"]; exists {
+	definition, exists := dico.GetDefinition("paris")
+	if exists {
 		fmt.Println("Définition de Paris:", definition)
 	} else {
 		fmt.Println("Paris non trouvé dans le dictionnaire.")
 	}
 
-	// Supprimer un élément de la carte
-	delete(dico_villes, "lisbonne")
+	// Supprimer un mot du dictionnaire
+	dico.RemoveWord("lisbonne")
 
-	// Afficher la carte après suppression
-	fmt.Println("Carte après suppression:", dico_villes)
-
-	// Lister les éléments de la carte
-	fmt.Println("Liste des éléments de la carte:")
-	for ville, pays := range dico_villes {
-		fmt.Printf("%s: %s\n", ville, pays)
-	}
+	// Afficher le dictionnaire après suppression
+	fmt.Println("Dictionnaire après suppression:")
+	dico.PrintDictionary()
 }
+
+
+
+
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	// Créer une carte pour stocker les villes et les pays
+// 	dico_villes := make(map[string]string)
+
+// 	// Ajouter des éléments à la carte
+// 	dico_villes["paris"] = "France"
+// 	dico_villes["lisbonne"] = "Portugal"
+// 	dico_villes["londres"] = "Angleterre"
+
+// 	// Afficher la carte
+// 	fmt.Println("Carte après ajout:", dico_villes)
+
+// 	// Utiliser la méthode Get pour obtenir la définition d'un mot spécifique
+// 	if definition, exists := dico_villes["paris"]; exists {
+// 		fmt.Println("Définition de Paris:", definition)
+// 	} else {
+// 		fmt.Println("Paris non trouvé dans le dictionnaire.")
+// 	}
+
+// 	// Supprimer un élément de la carte
+// 	delete(dico_villes, "lisbonne")
+
+// 	// Afficher la carte après suppression
+// 	fmt.Println("Carte après suppression:", dico_villes)
+
+// 	// Lister les éléments de la carte
+// 	fmt.Println("Liste des éléments de la carte:")
+// 	for ville, pays := range dico_villes {
+// 		fmt.Printf("%s: %s\n", ville, pays)
+// 	}
+// }
