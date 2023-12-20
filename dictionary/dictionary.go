dd4e1454601133ce3c5872e38e6dba84c98a58f6
celaireo <celaireo@gmail.com>
2023-12-20 11:24:13 +0100

    modification pour stockage de données dans un fichier json pour l' exercice 3

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,103 +1,62 @@
-
 package dictionary
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	//"os"
+)
 
 // Dictionary représente un dictionnaire
 type Dictionary struct {
-	words map[string]string
+	Words map[string]string `json:"words"`
 }
 
 // NewDictionary crée une nouvelle instance de dictionnaire
 func NewDictionary() *Dictionary {
 	return &Dictionary{
-		words: make(map[string]string),
+		Words: make(map[string]string),
 	}
 }
 
 // AddWord ajoute un mot et sa définition au dictionnaire
 func (d *Dictionary) AddWord(word, definition string) {
-	d.words[word] = definition
+	d.Words[word] = definition
 }
 
 // GetDefinition retourne la définition d'un mot spécifique
 func (d *Dictionary) GetDefinition(word string) (string, bool) {
-	definition, exists := d.words[word]
+	definition, exists := d.Words[word]
 	return definition, exists
 }
 
 // RemoveWord supprime un mot du dictionnaire
 func (d *Dictionary) RemoveWord(word string) {
-	delete(d.words, word)
+	delete(d.Words, word)
 }
 
 // PrintDictionary affiche tous les mots et leurs définitions dans le dictionnaire
 func (d *Dictionary) PrintDictionary() {
 	fmt.Println("Dictionnaire:")
-	for word, definition := range d.words {
+	for word, definition := range d.Words {
 		fmt.Printf("%s: %s\n", word, definition)
 	}
 }
 
+// SaveToFile enregistre les données du dictionnaire dans un fichier JSON
+func (d *Dictionary) SaveToFile(filename string) error {
+	data, err := json.MarshalIndent(d, "", "  ")
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(filename, data, 0644)
+}
 
-
-
-// package main
-
-// import (
-// 	"fmt"
-// )
-
-// func main() {
-// 	// Créer une carte pour stocker les villes et les pays
-// 	dico_villes := make(map[string]string)
-
-// 	// Ajouter des éléments à la carte
-// 	addToDictionary(dico_villes, "paris", "France")
-// 	addToDictionary(dico_villes, "lisbonne", "Portugal")
-// 	addToDictionary(dico_villes, "londres", "Angleterre")
-
-// 	// Afficher la carte
-// 	fmt.Println("Carte après ajout:", dico_villes)
-
-// 	// Utiliser la méthode Get pour obtenir la définition d'un mot spécifique
-// 	definition, exists := getFromDictionary(dico_villes, "paris")
-// 	if exists {
-// 		fmt.Println("Définition de Paris:", definition)
-// 	} else {
-// 		fmt.Println("Paris non trouvé dans le dictionnaire.")
-// 	}
-
-// 	// Supprimer un élément de la carte
-// 	removeFromDictionary(dico_villes, "lisbonne")
-
-// 	// Afficher la carte après suppression
-// 	fmt.Println("Carte après suppression:", dico_villes)
-
-// 	// Lister les éléments de la carte
-// 	listDictionary(dico_villes)
-// }
-
-// // Fonction pour ajouter un élément à la carte
-// func addToDictionary(dico map[string]string, ville string, pays string) {
-// 	dico[ville] = pays
-// }
-
-// // Fonction pour obtenir la définition d'un mot de la carte
-// func getFromDictionary(dico map[string]string, ville string) (string, bool) {
-// 	definition, exists := dico[ville]
-// 	return definition, exists
-// }
-
-// // Fonction pour supprimer un élément de la carte
-// func removeFromDictionary(dico map[string]string, ville string) {
-// 	delete(dico, ville)
-// }
-
-// // Fonction pour lister les éléments de la carte
-// func listDictionary(dico map[string]string) {
-// 	fmt.Println("Liste des éléments de la carte:")
-// 	for ville, pays := range dico {
-// 		fmt.Printf("%s: %s\n", ville, pays)
-// 	}
-// }
+// LoadFromFile charge les données du dictionnaire depuis un fichier JSON
+func (d *Dictionary) LoadFromFile(filename string) error {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(data, d)
+}
--- main.go
+++ main.go
@@ -13,10 +13,18 @@ func main() {
 	dico.AddWord("paris", "France")
 	dico.AddWord("lisbonne", "Portugal")
 	dico.AddWord("londres", "Angleterre")
+	dico.AddWord("abidjan", "Côte d'Ivoire")
 
 	// Afficher le dictionnaire
 	fmt.Println("Dictionnaire après ajout:")
 	dico.PrintDictionary()
+
+	// Enregistrer le dictionnaire dans un fichier JSON
+	err := dico.SaveToFile("dictionary.json")
+	if err != nil {
+		fmt.Println("Erreur lors de l'enregistrement du dictionnaire :", err)
+		return
+	}
 
 	// Utiliser la méthode Get pour obtenir la définition d'un mot spécifique
 	definition, exists := dico.GetDefinition("paris")
@@ -32,46 +40,15 @@ func main() {
 	// Afficher le dictionnaire après suppression
 	fmt.Println("Dictionnaire après suppression:")
 	dico.PrintDictionary()
+
+	// Charger le dictionnaire depuis le fichier JSON
+	err = dico.LoadFromFile("dictionary.json")
+	if err != nil {
+		fmt.Println("Erreur lors du chargement du dictionnaire :", err)
+		return
+	}
+
+	// Afficher le dictionnaire après chargement depuis le fichier
+	fmt.Println("Dictionnaire après chargement depuis le fichier:")
+	dico.PrintDictionary()
 }
-
-
-
-
-
-// package main
-
-// import (
-// 	"fmt"
-// )
-
-// func main() {
-// 	// Créer une carte pour stocker les villes et les pays
-// 	dico_villes := make(map[string]string)
-
-// 	// Ajouter des éléments à la carte
-// 	dico_villes["paris"] = "France"
-// 	dico_villes["lisbonne"] = "Portugal"
-// 	dico_villes["londres"] = "Angleterre"
-
-// 	// Afficher la carte
-// 	fmt.Println("Carte après ajout:", dico_villes)
-
-// 	// Utiliser la méthode Get pour obtenir la définition d'un mot spécifique
-// 	if definition, exists := dico_villes["paris"]; exists {
-// 		fmt.Println("Définition de Paris:", definition)
-// 	} else {
-// 		fmt.Println("Paris non trouvé dans le dictionnaire.")
-// 	}
-
-// 	// Supprimer un élément de la carte
-// 	delete(dico_villes, "lisbonne")
-
-// 	// Afficher la carte après suppression
-// 	fmt.Println("Carte après suppression:", dico_villes)
-
-// 	// Lister les éléments de la carte
-// 	fmt.Println("Liste des éléments de la carte:")
-// 	for ville, pays := range dico_villes {
-// 		fmt.Printf("%s: %s\n", ville, pays)
-// 	}
-// }
