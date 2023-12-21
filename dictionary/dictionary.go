0edb899330eda445aaef8b99bc18bdfca1850318
celaireo <celaireo@gmail.com>
2023-12-21 16:25:43 +0100

    API REST avec HTTP et gestion de routes en Go

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -2,10 +2,10 @@ package dictionary
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
-	"log"
+	"net/http"
 	"sync"
+	"strings"
 )
 
 type addOperation struct {
@@ -31,6 +31,12 @@ type readResponse struct {
 
 type dictionaryJSON struct {
 	Words map[string]string `json:"words"`
+}
+
+
+type addRequest struct {
+	Word       string `json:"word"`
+	Definition string `json:"definition"`
 }
 
 type Dictionary struct {
@@ -73,65 +79,197 @@ func (d *Dictionary) GetDefinition(word string) (string, bool) {
 	return resp.definition, resp.exists
 }
 
-func (d *Dictionary) PrintDictionary() {
+func (d *Dictionary) ListWords() map[string]string {
 	d.mu.RLock()
 	defer d.mu.RUnlock()
-	fmt.Println("Dictionnaire:")
+	result := make(map[string]string)
 	for word, definition := range d.words {
-		fmt.Printf("%s: %s\n", word, definition)
+		result[word] = definition
 	}
+	return result
 }
 
+
+// Sauvegarde dans un fichier JSON
 func (d *Dictionary) SaveToFile(filename string) error {
-	log.Println("Saving dictionary to file...")
-	close(d.saveCh)
+	// Envoie une structure vide via le canal saveCh pour notifier la goroutine principale.
+	d.saveCh <- struct{}{}
+
+	// Attend la confirmation de la goroutine principale.
 	<-d.saveCh
-	d.mu.RLock()
-	defer d.mu.RUnlock()
-	data, err := json.MarshalIndent(dictionaryJSON{Words: d.words}, "", "  ")
+
+	// code pour sauvegarder dans un fichier JSON
+	jsonData, err := json.Marshal(dictionaryJSON{Words: d.words})
 	if err != nil {
-		log.Println("Error marshalling dictionary:", err)
 		return err
 	}
-	err = ioutil.WriteFile(filename, data, 0644)
+
+	err = ioutil.WriteFile(filename, jsonData, 0644)
 	if err != nil {
-		log.Println("Error writing to file:", err)
-	} else {
-		log.Println("Dictionary saved successfully.")
+		return err
 	}
-	return err
+
+	return nil
 }
 
-func (d *Dictionary) LoadFromFile(filename string) error {
-	d.mu.Lock()
-	defer d.mu.Unlock()
-	data, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return err
-	}
-	return json.Unmarshal(data, &dictionaryJSON{Words: d.words})
-}
 
 func (d *Dictionary) processOperations() {
 	for {
 		select {
 		case addOp := <-d.addCh:
+			// ... (code pour traiter l'opération d'ajout)
 			d.mu.Lock()
 			d.words[addOp.word] = addOp.definition
 			d.mu.Unlock()
-			close(addOp.respCh)
-
+			addOp.respCh <- readResponse{} // Envoie une réponse vide pour notifier la fin de l'opération.
+		
 		case delOp := <-d.delCh:
+			// ... (code pour traiter l'opération de suppression)
 			d.mu.Lock()
 			delete(d.words, delOp.word)
 			d.mu.Unlock()
-			close(delOp.respCh)
-
+			delOp.respCh <- readResponse{} // Envoie une réponse vide pour notifier la fin de l'opération.
+		
 		case readOp := <-d.readCh:
+			// ... (code pour traiter l'opération de lecture)
 			d.mu.RLock()
 			definition, exists := d.words[readOp.word]
 			d.mu.RUnlock()
-			readOp.respCh <- readResponse{definition, exists}
+			readOp.respCh <- readResponse{definition, exists} // Envoie la réponse à l'opération de lecture.
+		
+		case <-d.saveCh:
+			// ... (code pour traiter l'opération de sauvegarde)
+			d.saveCh <- struct{}{} // Confirme la fin de l'opération de sauvegarde.
 		}
 	}
 }
+
+
+// Fonction pour gérer la route HTTP "/list"
+func (d *Dictionary) handleList(w http.ResponseWriter, r *http.Request) {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+
+	response := make(map[string]string)
+	for word, definition := range d.words {
+		response[word] = definition
+	}
+
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, "Error marshalling JSON", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResponse)
+}
+
+// Fonction pour gérer la route HTTP "/get"
+func (d *Dictionary) handleGet(w http.ResponseWriter, r *http.Request) {
+	word := r.URL.Path[len("/get/"):]
+	if word == "" {
+		http.Error(w, "Word parameter is required", http.StatusBadRequest)
+		return
+	}
+
+	definition, exists := d.GetDefinition(word)
+	if !exists {
+		http.Error(w, "Word not found in the dictionary", http.StatusNotFound)
+		return
+	}
+
+	response := map[string]string{"word": word, "definition": definition}
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, "Error marshalling JSON", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResponse)
+}
+
+
+// Fonction pour gérer la route HTTP "/add"
+func (d *Dictionary) handleAdd(w http.ResponseWriter, r *http.Request) {
+	// Vérifier que la méthode de la requête est POST
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Lire le corps de la requête
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+
+	// Désérialiser les données JSON
+	var addRequest addRequest
+	err = json.Unmarshal(body, &addRequest)
+	if err != nil {
+		http.Error(w, "Error unmarshalling JSON", http.StatusBadRequest)
+		return
+	}
+
+	// Vérifier que les champs nécessaires sont présents
+	if addRequest.Word == "" || addRequest.Definition == "" {
+		http.Error(w, "Word and definition are required fields", http.StatusBadRequest)
+		return
+	}
+
+	// Ajouter le mot au dictionnaire
+	d.AddWord(addRequest.Word, addRequest.Definition)
+
+	// Répondre avec un statut de succès
+	w.WriteHeader(http.StatusCreated)
+}
+
+// Fonction pour gérer la route HTTP "/remove"
+func (d *Dictionary) handleRemove(w http.ResponseWriter, r *http.Request) {
+	// Vérifier que la méthode de la requête est DELETE
+	if r.Method != http.MethodDelete {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Extraire le mot à supprimer de l'URL
+	word := r.URL.Path[len("/remove/"):]
+	if word == "" {
+		http.Error(w, "Word parameter is required", http.StatusBadRequest)
+		return
+	}
+
+	// Supprimer le mot du dictionnaire
+	d.RemoveWord(word)
+
+	// Répondre avec un statut de succès
+	w.WriteHeader(http.StatusOK)
+}
+
+// Gestion des routes HTTP
+func (d *Dictionary) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		if r.URL.Path == "/list" {
+			d.handleList(w, r)
+		} else if strings.HasPrefix(r.URL.Path, "/get/") {
+			d.handleGet(w, r)
+		} else {
+			http.Error(w, "Not Found", http.StatusNotFound)
+		}
+	case http.MethodPost:
+		d.handleAdd(w, r)
+	case http.MethodDelete:
+		d.handleRemove(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+	
+}
+
+	
+
+
--- main.go
+++ main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"dictionnaire/dictionary"
-	"fmt"
 	"log"
+	"net/http"
 	"sync"
-	"time"
+	"fmt"
 )
 
 func main() {
 	dico := dictionary.NewDictionary()
 
 	var wg sync.WaitGroup
+	serverDone := make(chan struct{})
 
 	wg.Add(1)
 	go func() {
@@ -20,6 +21,10 @@ func main() {
 		dico.AddWord("lisbonne", "Portugal")
 		dico.AddWord("londres", "Angleterre")
 		dico.AddWord("abidjan", "Côte d'Ivoire")
+		dico.AddWord("rio de janeiro", "Bresil")
+		dico.AddWord("rome", "Italy")
+		dico.AddWord("new york", "Etats-Unis")
+		dico.AddWord("tokyo", "Japon")
 	}()
 
 	wg.Add(1)
@@ -31,36 +36,32 @@ func main() {
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		dico.PrintDictionary()
+		dico.GetDefinition("paris")
 	}()
 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		time.Sleep(2 * time.Second)
 		err := dico.SaveToFile("dictionary.json")
 		if err != nil {
 			log.Println("Error saving dictionary to file:", err)
+			return
 		}
+		fmt.Println("Dictionary saved successfully!")
 	}()
+
+	go func() {
+		http.Handle("/", dico)
+		fmt.Println("Dictionnaire Server starting...")
+		log.Fatal(http.ListenAndServe(":8082", nil))
+		fmt.Println("Dictionnaire Server started successfully !")
+		close(serverDone) // Fermez le canal pour signaler que le serveur a terminé
+	}()
+
 
 	wg.Wait()
 
-	time.Sleep(1 * time.Second)
-
-	definition, exists := dico.GetDefinition("paris")
-	if exists {
-		fmt.Println("Définition de Paris:", definition)
-	} else {
-		fmt.Println("Paris non trouvé dans le dictionnaire.")
-	}
-
-	err := dico.LoadFromFile("dictionary.json")
-	if err != nil {
-		log.Println("Erreur lors du chargement du dictionnaire :", err)
-		return
-	}
-
-	fmt.Println("Dictionnaire après chargement depuis le fichier:")
-	dico.PrintDictionary()
+	// Attendre que le serveur ait terminé avant de quitter
+	<-serverDone
 }
+
